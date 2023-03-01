20dd65015cbccc62eea4af677fc0858faf1de1d4
swagoverlord <azcooldude8@gmail.com>
2023-03-01 12:26:18 +0100

    Formated code

--- Wall.java
+++ Wall.java
@@ -4,18 +4,49 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 interface Structure { 
-    // returns any element with the given color 
+    // Returns any element with the given color 
     Optional<Block> findBlockByColor(String color); 
     
-    // return all elements from the given material 
+    // Return all elements from the given material 
     List<Block> findBlocksByMaterial(String material); 
     
-    //returns the number of all elements in the structure 
+    // Returns the number of all elements in the structure 
     int count();
+
     } 
     
     public abstract class Wall implements Structure { 
-        private List<Block> blocks; 
+        private List<Block> blocks;
+
+    // Find block by color
+    public Optional<Block> findBlockByColor(String color) {
+        return blocks.stream()
+                .filter(block -> block.getColor().equals(color))
+                .findFirst();
+    }
+
+    // Find block by material
+    public List<Block> findBlocksByMaterial(String material) {
+        return blocks.stream()
+                .filter(block -> block.getMaterial().equals(material))
+                .collect(Collectors.toList());
+    }
+
+    public int count() {
+        return blocks.size();
+    }
+
+    public List<Block> getAllBlocks() {
+        List<Block> allBlocks = new ArrayList<>();
+        for (Block block : blocks) {
+            if (block instanceof CompositeBlock) {
+                allBlocks.addAll(((CompositeBlock) block).getBlocks());
+            } else {
+                allBlocks.add(block);
+            }
+        }
+        return allBlocks;
+    }
     } 
     
     interface Block { 
@@ -24,6 +55,6 @@ interface Structure {
     } 
     
     interface CompositeBlock extends Block { 
-    List<Block> getBlocks(); 
+        List<Block> getBlocks(); 
     } 
 
